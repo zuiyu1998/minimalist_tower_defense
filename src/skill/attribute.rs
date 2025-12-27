557401c2560dfdaf03e0f9a93d42ba343945fabf
zuiyu1998 <1542844298@qq.com>
2025-12-27 17:42:47 +0800

    feat: 添加 SkillAttributeSet

--- src/skill/attribute.rs
+++ src/skill/attribute.rs
@@ -1,6 +1,81 @@
-use std::f32;
+use std::{f32, mem::swap};
+
+use bevy::{ecs::component::Component, platform::collections::HashMap};
+
+#[derive(Debug, Default, Component)]
+pub struct SkillAttributeSet {
+    data: HashMap<String, SkillAttribute>,
+    dependency_modifiers: HashMap<String, AttributeDependencyModifier>,
+}
+
+impl SkillAttributeSet {
+    pub fn skill_attribute(&self, attribute_name: &str) -> Option<&SkillAttribute> {
+        self.data.get(attribute_name)
+    }
+
+    pub fn add_dependency_modifier(&mut self, dependency_modifier: AttributeDependencyModifier) {
+        let source = dependency_modifier.get_source();
+        self.dependency_modifiers
+            .insert(source, dependency_modifier);
+    }
+
+    pub fn add_skill_attribute(&mut self, attribute: SkillAttribute) {
+        let attribute_name = attribute.name.clone();
+        self.data.insert(attribute_name, attribute);
+    }
+
+    pub fn update_attribute_base_value(&mut self, attribute_name: &str, value: f32) {
+        if let Some(attribute) = self.data.get_mut(attribute_name) {
+            let old_value = attribute.get_current_value();
+
+            attribute.update_base_value(value);
+
+            let new_value = attribute.get_current_value();
+
+            if new_value != old_value {
+                let dependency_modifiers = self
+                    .dependency_modifiers
+                    .values()
+                    .filter(|dependency_modifier| dependency_modifier.source == attribute_name)
+                    .collect::<Vec<_>>();
+
+                for dependency_modifier in dependency_modifiers.iter() {
+                    if let Some(target_attribute) = self.data.get_mut(&dependency_modifier.target) {
+                        dependency_modifier.update(target_attribute, new_value - old_value);
+                    } else {
+                        tracing::error!("{} attribute_name not found.", dependency_modifier.target);
+                    }
+                }
+            }
+        } else {
+            tracing::error!("{} attribute_name not found.", attribute_name);
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct AttributeDependencyModifier {
+    source: String,
+    target: String,
+    value: f32,
+}
+
+impl AttributeDependencyModifier {
+    pub fn get_source(&self) -> String {
+        format!("__attribute_dependency_{}__{}", self.source, self.target)
+    }
+
+    pub fn update(&self, target: &mut SkillAttribute, value: i32) {
+        let mut modifier = SkillAttributeModifier::default();
+        modifier.source = self.get_source();
+        modifier.value = self.value * (value as f32);
+
+        target.add_modifier(&modifier);
+    }
+}
 
 ///属性
+#[derive(Debug, Clone)]
 pub struct SkillAttribute {
     ///物理的最小值
     min_value: f32,
@@ -12,6 +87,8 @@ pub struct SkillAttribute {
     current_value: f32,
     ///修改器类别
     modifiers: Vec<SkillAttributeModifier>,
+    ///属性名
+    pub name: String,
 }
 
 impl Default for SkillAttribute {
@@ -22,11 +99,25 @@ impl Default for SkillAttribute {
             base_value: 0.0,
             current_value: 0.0,
             modifiers: vec![],
+            name: Default::default(),
         }
     }
 }
 
 impl SkillAttribute {
+    pub fn remove_modifier_with_source(&mut self, source: &str) {
+        let mut modifiers = vec![];
+
+        swap(&mut self.modifiers, &mut modifiers);
+
+        self.modifiers = modifiers
+            .into_iter()
+            .filter(|modifier| modifier.source != source)
+            .collect::<Vec<_>>();
+
+        self.calculate_current_value();
+    }
+
     pub fn add_modifier(&mut self, modifier: &SkillAttributeModifier) {
         if !self.modifiers.contains(modifier) {
             self.modifiers.push(modifier.clone());
@@ -136,7 +227,44 @@ pub struct SkillAttributeModifier {
 
 #[cfg(test)]
 mod test {
-    use super::{SkillAttribute, SkillAttributeModifier, SkillAttributeModifierOperation};
+
+    use super::{
+        AttributeDependencyModifier, SkillAttribute, SkillAttributeModifier,
+        SkillAttributeModifierOperation, SkillAttributeSet,
+    };
+
+    #[test]
+    fn test_attribute_set() {
+        let mut attribute_set = SkillAttributeSet::default();
+
+        let mut attribute = SkillAttribute::default();
+        attribute.name = "power".to_string();
+        attribute.update_base_value(10.0);
+
+        attribute_set.add_skill_attribute(attribute);
+
+        let mut attribute = SkillAttribute::default();
+        attribute.name = "burden".to_string();
+        attribute.update_base_value(10.0);
+
+        attribute_set.add_skill_attribute(attribute);
+
+        let mut dependency_modifier = AttributeDependencyModifier::default();
+
+        dependency_modifier.source = "power".to_string();
+        dependency_modifier.target = "burden".to_string();
+        dependency_modifier.value = 10.0;
+
+        attribute_set.add_dependency_modifier(dependency_modifier);
+
+        attribute_set.update_attribute_base_value("power", 1.0);
+
+        let burden_attribute = attribute_set.skill_attribute("burden").unwrap();
+
+        let burden = burden_attribute.get_current_value();
+
+        assert_eq!(20, burden);
+    }
 
     #[test]
     fn test_attribute() {
@@ -166,5 +294,9 @@ mod test {
         overload_modifier1.priority = 10;
         attribute.add_modifier(&overload_modifier1);
         assert_eq!(attribute.get_current_value(), 20);
+
+        attribute.remove_modifier_with_source(&overload_modifier1.source);
+
+        assert_eq!(attribute.get_current_value(), 10);
     }
 }
