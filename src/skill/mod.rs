509822bed660df51b75a07fb1937b86a92965f11
zuiyu1998 <1542844298@qq.com>
2025-12-31 16:02:14 +0800

    feat: 添加process_skill_effct

--- src/skill/mod.rs
+++ src/skill/mod.rs
@@ -1,7 +1,76 @@
 mod attribute;
 
+use std::fmt::Debug;
+
 pub use attribute::*;
 
-use bevy::prelude::*;
+use bevy::{ecs::system::SystemParam, platform::collections::HashMap, prelude::*};
 
-pub(super) fn plugin(_app: &mut App) {}
+#[derive(Debug, SystemParam)]
+pub struct SkillSystemParam {}
+
+///技能运行过程数据
+#[derive(Debug, Component)]
+pub struct SkillRunContext {
+    pub source: Option<Entity>,
+    pub caster: Entity,
+    pub target: Entity,
+    pub payload: HashMap<String, Box<dyn SkillData>>,
+}
+
+pub trait SkillData: 'static + Sync + Send + Debug {}
+
+pub struct Skill {}
+
+pub trait FromSkill {
+    fn from_skill(skill: &Skill) -> Self;
+}
+
+#[derive(Debug, SystemParam)]
+pub struct MySystemParam {}
+
+#[derive(Debug, Component)]
+pub struct MySkillEffect {}
+
+impl FromSkill for MySkillEffect {
+    fn from_skill(_skill: &Skill) -> Self {
+        MySkillEffect {}
+    }
+}
+
+impl SkillEffctProcessor for MySystemParam {
+    type Effect = MySkillEffect;
+}
+
+pub struct SkillResponse {}
+
+impl SkillResponse {
+    pub fn empty() -> SkillResponse {
+        SkillResponse {}
+    }
+}
+
+pub trait SkillEffctProcessor {
+    type Effect: FromSkill + Component;
+
+    fn process(
+        &self,
+        _skill_effct: &Self::Effect,
+        _context: &mut SkillRunContext,
+    ) -> SkillResponse {
+        SkillResponse::empty()
+    }
+}
+
+pub fn process_skill_effct<T: SystemParam + SkillEffctProcessor>(
+    processor: T,
+    mut skill_effct_q: Query<(&T::Effect, &mut SkillRunContext)>,
+) {
+    for (skill_effct, mut context) in skill_effct_q.iter_mut() {
+        processor.process(skill_effct, &mut context);
+    }
+}
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_systems(Last, process_skill_effct::<MySystemParam>);
+}
