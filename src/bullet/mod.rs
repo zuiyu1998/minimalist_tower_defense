dde3ccbeec83eb343b2cf3030f79eecdfa1ca5ad
zuiyu1998 <1542844298@qq.com>
2026-03-16 16:03:50 +0800

    feat: 添加子弹自动销毁

--- src/bullet/mod.rs
+++ src/bullet/mod.rs
@@ -4,10 +4,27 @@ use bevy::{platform::collections::HashSet, prelude::*};
 use crate::common::{GameLayer, Stas, spawn_hit};
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(Update, on_bullet_attack);
+    app.add_systems(Update, (bullet_attack, queue_free).chain());
 }
 
-fn apply(
+#[derive(Debug, Component)]
+pub struct StartPosition(Vec2);
+
+//子弹运行一定距离后自动删除
+fn queue_free(
+    mut commands: Commands,
+    bullet_q: Query<(&Transform, Entity, &StartPosition), With<Bullet>>,
+) {
+    for (transform, entity, start) in bullet_q.iter() {
+        let distance = transform.translation.truncate().distance(start.0);
+
+        if distance > 1000.0 {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn on_bullet_attack(
     stats_q: &mut Query<&mut Stas>,
     bullet_q: &Query<(&Bullet, Entity)>,
     bullet_entity: Entity,
@@ -27,7 +44,7 @@ fn apply(
     }
 }
 
-fn on_bullet_attack(
+fn bullet_attack(
     mut commands: Commands,
     mut collision_reader: MessageReader<CollisionStart>,
     bullet_q: Query<(&Bullet, Entity)>,
@@ -42,8 +59,8 @@ fn on_bullet_attack(
         let body1 = event.body1.clone().unwrap();
         let body2 = event.body2.clone().unwrap();
 
-        apply(&mut stats_q, &bullet_q, body1, body2, &mut die_set);
-        apply(&mut stats_q, &bullet_q, body2, body1, &mut die_set);
+        on_bullet_attack(&mut stats_q, &bullet_q, body1, body2, &mut die_set);
+        on_bullet_attack(&mut stats_q, &bullet_q, body2, body1, &mut die_set);
     }
 
     for entity in die_set.iter() {
@@ -76,6 +93,7 @@ pub fn spawn_bullet(
             translation: Vec3::new(bullet_position.x, bullet_position.y, 0.0),
             ..default()
         },
+        StartPosition(bullet_position),
     ));
 
     spawn_hit(&mut commands, collider, layers);
